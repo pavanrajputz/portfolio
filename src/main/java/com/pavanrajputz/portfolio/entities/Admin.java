fd5c2468b623bb157d74145752252eabd2ef582e
pavanrajputz <pr7676150@gmail.com>
2026-09-04 16:19:06 +0530

    created teh admin entity and also introduced env added to gitignore now configuring the security

--- src/main/java/com/pavanrajputz/portfolio/entities/Admin.java
+++ src/main/java/com/pavanrajputz/portfolio/entities/Admin.java
@@ -1,16 +1,24 @@
 package com.pavanrajputz.portfolio.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Table(name = "admins")
 @Entity
 public class Admin {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false, unique = true)
+    private String email;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
---
+++ src/main/java/com/pavanrajputz/portfolio/entities/Role.java
@@ -0,0 +1,6 @@
+package com.pavanrajputz.portfolio.entities;
+
+public enum Role {
+    ADMIN,
+    USER,
+}
