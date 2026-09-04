a9a587330ca89c99adb893f9343b6ab44e3bc7c8
pavanrajputz <pr7676150@gmail.com>
2026-09-04 10:48:43 +0530

    starting with project

---
+++ src/main/java/com/pavanrajputz/portfolio/entities/Admin.java
@@ -0,0 +1,16 @@
+package com.pavanrajputz.portfolio.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+}
