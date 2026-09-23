24a71074b231c06bd97e38039bd7cc2248f57cad
pavanrajputz <pr7676150@gmail.com>
2026-09-23 22:30:53 +0530

    added the grantedatutherity

--- src/main/java/com/pavanrajputz/portfolio/entities/Admin.java
+++ src/main/java/com/pavanrajputz/portfolio/entities/Admin.java
@@ -3,6 +3,7 @@ package com.pavanrajputz.portfolio.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -29,7 +30,11 @@ public class Admin implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(
+                new SimpleGrantedAuthority(
+                        "ROLE_" + role.name()
+                )
+        );
     }
 
     @Override
