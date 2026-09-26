1ee9bbce38dc08350b57e738c70e8eea890bda7f
pavanrajputz <pr7676150@gmail.com>
2026-09-27 00:40:11 +0530

    added one tiral test and now implement the autest for application

---
+++ src/test/java/com/pavanrajputz/portfolio/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.pavanrajputz.portfolio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    @Test
+    void shouldAddTwoNumber(){
+        int result = 2+3;
+        assertEquals(5, result);
+    }
+}
